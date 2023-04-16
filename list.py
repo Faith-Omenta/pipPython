67434f0931940be5ca802a5cbb6079e49ff46888
Faith-Omenta <faithbomenta@gmail.com>
2023-04-16 18:58:39 +0300

    pythn

---
+++ list.py
@@ -0,0 +1,30 @@
+# .reverse()
+# list1 = list1[::-1]
+list=[200,300,400,500,667,756]
+list.reverse()
+print(list)
+
+# length
+my_list=[3,2,4,5,6,6,7]
+print(len(my_list))
+
+# index
+listOf=["Melvin",234,"Glenah",343,34.7]
+print(listOf[2])
+print(listOf[4])
+print(listOf[-2])
+print(listOf[-4])
+
+# slicing
+lis=["Melvin",234,"Glenah",343,34.7,"Philis"]
+print(lis[1:4])
+print(lis[:4])
+print(lis[3:])
+print(lis[::-3])
+
+# iterating a list using a for loop
+lists=["Melvin",234,"Glenah",343,34.7,"Philis"]
+for item in lists:
+    print(item)
+
+
